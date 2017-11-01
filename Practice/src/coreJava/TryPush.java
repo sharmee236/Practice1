0632d0615b5603feede2d7955cc575e16030251c
Sharmin <sharmee236@yahoo.com>
2017-11-01 13:07:35 -0400

    new add

---
+++ Practice/src/coreJava/TryPush.java
@@ -0,0 +1,14 @@
+package coreJava;
+
+public class TryPush {
+
+	public static void main(String[] args) {
+		
+		TryPush TP= new TryPush();
+		TP.equals(null);
+		System.out.println(TP);
+		System.out.println("New push to git");
+
+	}
+
+}
